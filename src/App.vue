c9e60e8d2292cf3333cb37ae478f7ed07ab28dc6
marcobusacca <marcobusacca01@gmail.com>
2023-08-29 17:04:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+export default {
+
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use './styles/generals.scss' as *;
+</style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+// IMPORTO REACTIVE DA VUE
+import { reactive } from "vue";
+
+// ESPORTO LA CONSTANTE STORE CONTENENTE TUTTI GLI ELEMENTI ACCESSIBILI AD OGNI COMPONENTE VUE
+export const store = reactive({
+});
